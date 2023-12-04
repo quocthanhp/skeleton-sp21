cc3e0cd06b0cdcb33d9673e7df8b2d708f521489
quocthanhp <phamquocthanh1502@gmail.com>
2023-12-05 09:53:57 +1100

    Add extra methods

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,21 +1,23 @@
 package deque;
 
-public class ArrayDeque<Item> {
-    private Item[] items;
+import java.util.Iterator;
+
+public class ArrayDeque<T> implements Iterable<T> {
+    private T[] items;
     private int size;
     private int nextFirst;
     private int nextLast;
     private int len = 8;
 
     public ArrayDeque() {
-        items = (Item[]) new Object[len];
+        items = (T[]) new Object[len];
         size = 0;
         nextFirst = 4;
         nextLast = 5;
     }
 
     /** Adds an item to the front of the deque */
-    public void addFirst(Item item) {
+    public void addFirst(T item) {
         if (size == len) {
             resize(size * 2);
         }
@@ -26,7 +28,7 @@ public class ArrayDeque<Item> {
     }
 
     /** Adds an item to the back of the deque */
-    public void addLast(Item item) {
+    public void addLast(T item) {
         if (size == len) {
             resize(size * 2);
         }
@@ -38,7 +40,7 @@ public class ArrayDeque<Item> {
 
     /** Resize the array to size capacity */
     private void resize(int capacity) {
-        Item[] a = (Item[]) new Object[capacity];
+        T[] a = (T[]) new Object[capacity];
         int itemsFromFront = len - nextFirst - 1;
         len = capacity;
         System.arraycopy(items, 0, a, 0, nextLast);
@@ -74,13 +76,13 @@ public class ArrayDeque<Item> {
     }
 
     /** Removes and returns the item at the front of the deque. If no such item exists, returns null */
-    public Item removeFirst() {
+    public T removeFirst() {
         if (size == 0) {
             return null;
         }
 
         int removedIndex = (nextFirst + 1) % len;
-        Item removedItem = items[removedIndex];
+        T removedItem = items[removedIndex];
 
 
         items[removedIndex] = null;
@@ -91,13 +93,13 @@ public class ArrayDeque<Item> {
     }
 
     /** Removes and returns the item at the back of the deque. If no such item exists, returns null */
-    public Item removeLast() {
+    public T removeLast() {
         if (size == 0) {
             return null;
         }
 
         int removedIndex = (nextLast - 1) < 0 ? len - 1 : nextLast - 1;
-        Item removedItem = items[removedIndex];
+        T removedItem = items[removedIndex];
         items[removedIndex] = null;
         size--;
         nextLast = removedIndex;
@@ -108,7 +110,7 @@ public class ArrayDeque<Item> {
     /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
      * If no such item exists, returns null.
      */
-    public Item get(int index) {
+    public T get(int index) {
         if (index < 0 || index >= size) {
             return null;
         }
@@ -117,5 +119,60 @@ public class ArrayDeque<Item> {
 
         return items[getIndex];
     }
+
+    public Iterator<T> iterator() {
+        return new ArrayDequeIterator();
+    }
+
+    private class ArrayDequeIterator implements Iterator<T> {
+        private int pos = 0;
+
+        @Override
+        public boolean hasNext() {
+            return pos < size;
+        }
+        @Override
+        public T next() {
+            T returnItem = get(pos);
+            pos++;
+            return returnItem;
+        }
+    }
+
+    private boolean contains(T item) {
+        for (T i : items) {
+            if (i == null) {
+                continue;
+            }
+
+            if (i.equals(item)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o instanceof ArrayDeque ad) {
+            if (ad.size != this.size) {
+                return false;
+            }
+
+            for (T x : this) {
+                if (!ad.contains(x)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
 
 }
